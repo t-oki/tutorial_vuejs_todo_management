9c96ba7900240fb0df2312e8c7a7577d2eea83af
t-oki <blutac.nz@gmail.com>
2019-01-13 18:56:17 +0900

    create new todo list

--- src/store.js
+++ src/store.js
@@ -1,16 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    todoList:[
+      { id: 1, val: "みかん", checked: false },
+      { id: 2, val: "りんご", checked: false }
+    ],  
   },
   mutations: {
-
+    setTodoList(state, list){
+      state.todoList = list
+    }
   },
   actions: {
 
-  }
+  },
+  plugins: [createPersistedState()]
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,71 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <div>
+      <input v-model="text" type="text" class="input" @keypress.enter="onSubmit">
+      <button class="button" @click="onSubmit">Submit</button>
+      <ul>
+        <li v-for="(item, i) in list"　:key="item.id">
+          <input v-model="item.checked" type="checkbox">
+          <span>{{ item.val }}</span>
+          <button class="button" @click="onClickDelete(i)">Delete</button>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <button class="button" @click="onClickDeleteChecked">Delete Checked</button>
+      <button class="button" @click="onClickDeleteAll">Delete All</button>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+      list: this.$store.state.todoList,
+      id: 2
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (this.text) {
+        this.id += 1;
+        console.log(this.id);
+        this.list.push({
+          id: this.id,
+          val: this.text,
+          checked: false
+        });
+        this.text = "";
+        this.updateStore();
+      }
+    },
+    onClickDelete(i) {
+      this.list.pop(i);
+      this.updateStore();
+    },
+    onClickDeleteChecked() {
+      this.list = this.list.filter(item => !item.checked);
+      this.updateStore();
+    },
+    onClickDeleteAll() {
+      this.list = [];
+      this.updateStore();
+    },
+    updateStore() {
+      this.$store.commit("setTodoList", this.list);
+    }
+  }
+};
+</Script>
+
+<style lang="scss">
+#about {
+  .input {
+    border: 1px solid black;
+  }
+}
+</style>
+
